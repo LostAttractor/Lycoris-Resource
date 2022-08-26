8052d5b54e7b14e45595c8aa527ea423d2e84b66
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2022-08-26 20:55:31 +0800

    test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,14 @@
-import java.io.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.FileWriter
+import java.io.IOException
 import java.math.BigInteger
 import java.net.HttpURLConnection
 import java.net.URL
+import java.nio.channels.Channels
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.security.MessageDigest
-import java.util.stream.Collectors
 
 
 var runPath: Path = Paths.get("").toAbsolutePath()
@@ -19,27 +22,50 @@ fun main(args: Array<String>) {
             for (arg in args) {
                 if (f.name.endsWith(".$arg")) {
                     println("Found a File: ${f.name}")
-                    val fis = FileInputStream(f)
                     val hashAlgorithm = HashAlgorithm.MD5
-                    val buffer = ByteArray(fis.available())
-                    fis.read(buffer)
-                    fis.close()
+                    val stream = f.inputStream()
+                    val buffer = ByteArray(stream.available())
+                    stream.read(buffer)
+                    stream.close()
                     val hash = getHashCode(buffer, hashAlgorithm)
-                    println("${hashAlgorithm.modeName}: $hash")
+                    println("${hashAlgorithm.modeName}: $hash, ${buffer.size}")
                     writeFileString(file.resolve("${f.name}.${hashAlgorithm.name.lowercase()}"), hash)
                 }
             }
-//            val hashOnline = getHashCode(getFileString("https://srgmaps.vercel.app/${f.name}"), hashMode)
-//            println(hashOnline)
         }
     }
+//    val hashAlgorithm = HashAlgorithm.MD5
+//    val stream = BufferedInputStream(URL("https://github.com/LostAttractor/SRGMaps/blob/main/VANILLA189.srg?raw=true").openStream())
+//   // val connection = createConnection("https://github.com/LostAttractor/SRGMaps/blob/main/VANILLA189.srg?raw=true")
+//    //val stream = connection.inputStream
+//    val bufferStream = ByteArrayOutputStream()
+//    val buffer = ByteArray(1024)
+//    var count: Int
+//    while (stream.read(buffer, 0, 1024).also { count = it } != -1) {
+//        bufferStream.write(buffer, 0, count)
+//    }
+//    stream.close();
+//    println(bufferStream.toByteArray().size)
+//    val hash = getHashCode(buffer, hashAlgorithm)
+//    println("${hashAlgorithm.modeName}: $hash")
+    downloadUsingNIO("https://github.com/LostAttractor/SRGMaps/blob/main/VANILLA189.srg?raw=true", "VANILLA189.srg.test")
 }
 
-fun getFileString(url: String): String {
-    val connection = createConnection(url)
-    val reader = BufferedReader(InputStreamReader(connection.inputStream))
-    return reader.lines().collect(Collectors.joining(System.lineSeparator()))
+@Throws(IOException::class)
+private fun downloadUsingNIO(urlStr: String, file: String) {
+    val url = URL(urlStr)
+    val rbc = Channels.newChannel(url.openStream())
+    val fos = FileOutputStream(file)
+    fos.channel.transferFrom(rbc, 0, Long.MAX_VALUE)
+    fos.close()
+    rbc.close()
 }
+
+//fun getFileString(url: String): String {
+//    val connection = createConnection(url)
+//    val reader = BufferedReader(InputStreamReader(connection.inputStream))
+//    return reader.lines().collect(Collectors.joining(System.lineSeparator()))
+//}
 
 private fun createConnection(url: String): HttpURLConnection {
     val connection = URL(url).openConnection() as HttpURLConnection
---
+++ src/main/kotlin/RM.kt
@@ -0,0 +1,30 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.stream.Collectors
+
+
+fun main(args: Array<String>) {
+    //println(System.lineSeparator()[2].code)
+    println("Running Path: $runPath")
+    val file = runPath.toFile() //获取其file对象
+    val fs = file.listFiles() //遍历path下的文件和目录，放在File数组中
+    for (f in fs!!) {                    //遍历File[]数组
+        if (!f.isDirectory) {
+                if (f.name.endsWith(".srg")) {
+                    println("Found a File: ${f.name}")
+                    val stream = f.inputStream()
+                    val buffer = ByteArray(stream.available())
+                    stream.read(buffer)
+                    String(buffer).split(System.lineSeparator())[1].forEach { println("it: $it code: ${it.code}") }
+                    return
+//                    val reader = BufferedReader(InputStreamReader(stream))
+//                    val lines = reader.lines().collect(Collectors.joining(System.lineSeparator()))
+//                    val stringBuilder  = StringBuilder()
+//                    for (s in lines.split(System.lineSeparator()).dropLastWhile { it.isEmpty() }) {
+//                        stringBuilder.append(s.dropLastWhile { it.code == 13 }).append(System.lineSeparator())
+//                    }
+//                    writeFileString(file.resolve("${f.name}.test"), stringBuilder.toString())
+                }
+        }
+    }
+}
